5da8cec5e129b9f4cc13b0cf0c1675e662a41b3a
Wildan404 <85572458+Wildan404@users.noreply.github.com>
2021-06-10 05:02:53 +0700

    Add files via upload

---
+++ Kid.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Kid extends Overriding{
+    public static void main(String[] args){
+        Kid data = new Kid();
+        System.out.println("Makanan haram = "+ data.ambilA());
+        System.out.println("Makanan halal = "+ data.ambilB());
+    }
+}
---
+++ Overriding.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Overriding {
+
+    String A = "Babi";
+    String B = "Ayam";
+
+    String ambilA() {
+        return A;
+    }
+
+    String ambilB() {
+        return B;
+    }
+}
